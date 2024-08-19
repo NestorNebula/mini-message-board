c3a67ecc0a21c1124252cc6bf4d042143b6f0e3f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-19 11:35:59 +0200

    feat: add basic indexRouter to the page

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const path = require('node:path');
 require('dotenv').config();
 const app = express();
+const indexRouter = require('./routes/indexRouter');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT);
