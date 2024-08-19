cbfd3fb246d024e3d043572d5a3df3dfe1c7cfde
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-19 15:13:21 +0200

    feat: add /new post logic

--- routes/newRouter.js
+++ routes/newRouter.js
@@ -3,5 +3,13 @@ const newRouter = express.Router();
 const { messages } = require('./indexRouter');
 
 newRouter.get('/', (req, res) => res.render('form'));
+newRouter.post('/', (req, res) => {
+  const messageUser = req.body.author;
+  const messageText = req.body.message;
+  if (messageUser && messageText) {
+    messages.push({ text: messageText, user: messageUser, added: new Date() });
+  }
+  res.redirect('/');
+});
 
 module.exports = newRouter;
