4d30a81eb162bcb55895f329460c81a8c9f942de
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-19 18:02:13 +0200

    feat: update Date format

--- controllers/newController.js
+++ controllers/newController.js
@@ -6,7 +6,11 @@ const postNew = (req, res) => {
   const messageUser = req.body.author;
   const messageText = req.body.message;
   if (messageUser && messageText) {
-    messages.push({ text: messageText, user: messageUser, added: new Date() });
+    messages.push({
+      text: messageText,
+      user: messageUser,
+      added: new Date().toLocaleDateString('en'),
+    });
   }
   res.redirect('/');
 };
