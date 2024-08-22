28dda666f82c70549465ddac21d69fbff6644819
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-22 15:55:27 +0200

    refactor: update pool to use production db

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 module.exports = new Pool({
-  connectionString: process.env.LOCAL_DB,
+  connectionString: process.env.PRODUCTION_DB,
 });
