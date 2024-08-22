e50427ddaedd4a70c1d3400a23b037dbee7cf8b7
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-22 15:17:19 +0200

    feat: add form validation with express-validator

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,18 +1,27 @@
 const db = require('../db/queries');
+const { body } = require('express-validator');
+
+const validateMessage = [
+  body('author').trim().escape(),
+  body('message').trim().blacklist('<>/'),
+];
 
 const getNew = (req, res) => res.render('form');
 
-const postNew = async (req, res) => {
-  const messageUser = req.body.author;
-  const messageText = req.body.message;
-  if (messageUser && messageText) {
-    db.insertMessage({
-      user: messageUser,
-      text: messageText,
-      added: new Date().toISOString().split('T')[0],
-    });
-  }
-  res.redirect('/');
-};
+const postNew = [
+  validateMessage,
+  async (req, res) => {
+    const messageUser = req.body.author;
+    const messageText = req.body.message;
+    if (messageUser && messageText) {
+      await db.insertMessage({
+        user: messageUser,
+        text: messageText,
+        added: new Date().toISOString().split('T')[0],
+      });
+    }
+    res.redirect('/');
+  },
+];
 
 module.exports = { getNew, postNew };
