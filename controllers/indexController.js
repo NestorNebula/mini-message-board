8df573f1d55f3e9d6941a08e3e1a02e6d9d6260c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-22 14:05:11 +0200

    feat: add controller for the default path

---
+++ controllers/indexController.js
@@ -0,0 +1,10 @@
+const db = require('../db/queries');
+
+async function getMessages(req, res) {
+  const messages = await db.getAllMessages();
+  res.render('index', { messages: messages });
+}
+
+module.exports = {
+  getMessages,
+};
