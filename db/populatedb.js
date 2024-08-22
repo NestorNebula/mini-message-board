8a023f934919dfb416786dcc199387db60ae6189
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-22 11:37:30 +0200

    feat: add the function to populate db

---
+++ db/populatedb.js
@@ -0,0 +1,27 @@
+const { Client } = require('pg');
+require('dotenv').config();
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user VARCHAR(30),
+  message VARCHAR(255),
+  date DATE
+);
+
+INSERT INTO messages (user, message, date)
+VALUES
+  ('Amando', 'Hi there!', '2024-08-22'),
+  ('Charles', 'Hello World!', '2024-08-22');
+`;
+
+async function main() {
+  const client = new Client({
+    connectionString: process.env.LOCAL_DB,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+}
+
+main();
