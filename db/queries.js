1fdcd508afb245b512c271ee0ee31dbf8709eddd
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-22 12:00:04 +0200

    feat: add db queries functions

---
+++ db/queries.js
@@ -0,0 +1,23 @@
+const pool = require('./pool');
+
+async function getAllMessages() {
+  const { rows } = await pool.query('SELECT * FROM messages');
+  return rows;
+}
+
+async function insertMessage(message) {
+  await pool.query(
+    'INSERT INTO messages (username, message, date) VALUES ($1, $2, $3)',
+    [message.user, message.text, message.added]
+  );
+}
+
+async function getMessage(id) {
+  await pool.query('SELECT * FROM messages WHERE id = $1', [id]);
+}
+
+module.exports = {
+  getAllMessages,
+  insertMessage,
+  getMessage,
+};
